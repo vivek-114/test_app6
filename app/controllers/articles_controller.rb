3545b473f84523ffa57e7b88aa17522c365f56b6
vivek-114 <vivekvelugubantla114@gmail.com>
2020-11-06 07:50:46 +0530

    add one to many association btw users to articles from backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,8 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-
+    @article.user = User.first
+    #byebug
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
 end
